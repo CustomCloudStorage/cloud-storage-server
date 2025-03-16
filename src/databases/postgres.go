6eb796dd728c09cb116fda02799f5d894046dd16
A.Dragosh <sd9265479529@gmail.com>
2025-03-17 00:13:03 +0300

    chore(database): настройка системы миграций

--- src/databases/postgres.go
+++ src/databases/postgres.go
@@ -3,7 +3,11 @@ package databases
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 )
 
@@ -27,5 +31,30 @@ func GetDB(cfg PostgresConfig) (*sql.DB, error) {
 		return nil, err
 	}
 
+	if err := RunMigrations(db, "./migrations"); err != nil {
+		log.Fatalf("Could not run migrations: %v", err)
+	}
+
 	return db, nil
+}
+
+func RunMigrations(db *sql.DB, migrationsPath string) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("could not create migration driver: %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://"+migrationsPath,
+		"postgres", driver)
+	if err != nil {
+		return fmt.Errorf("could not create migration instance: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("could not run migrations: %w", err)
+	}
+
+	log.Println("Migrations applied successfully")
+	return nil
 }
