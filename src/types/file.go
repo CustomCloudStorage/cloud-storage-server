fad25d626659b8b5159a1a924179895c56743c96
A.Dragosh <sd9265479529@gmail.com>
2025-04-06 14:13:52 +0300

    refactor(types): разделил storage.go на file.go и folder.go

---
+++ src/types/file.go
@@ -0,0 +1,15 @@
+package types
+
+import "time"
+
+type File struct {
+	ID           int        `json:"id" gorm:"primaryKey;column:id"`
+	UserID       int        `json:"user_id" gorm:"not null;column:user_id"`
+	FolderID     *int       `json:"folder_id,omitempty" gorm:"column:folder_id"`
+	Name         string     `json:"name" gorm:"not null;column:name"`
+	Size         int64      `json:"size" gorm:"not null;column:size"`
+	PhysicalName string     `json:"physical_name" gorm:"not null;column:physical_name"`
+	CreatedAt    time.Time  `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt    time.Time  `json:"updated_at" gorm:"column:updated_at"`
+	DeletedAt    *time.Time `json:"deleted_at,omitempty" gorm:"column:deleted_at"`
+}
